35b7d4258e7fd82d4c4058041ef26e7c35c18e4b
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-10-25 00:47:58 +0600

    add tailwind firebase router and other packges

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBR-E0hbom-Ow0jErj4KXHf0cvVfPmQy1Q",
+    authDomain: "secret-web-1e169.firebaseapp.com",
+    projectId: "secret-web-1e169",
+    storageBucket: "secret-web-1e169.appspot.com",
+    messagingSenderId: "887827226445",
+    appId: "1:887827226445:web:38bad408d1b69ca0e4a104"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
