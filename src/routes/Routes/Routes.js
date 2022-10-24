676d10e388ce80fd2324f8e05729a38fd1717ae8
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-10-25 03:24:00 +0600

    add Navbar and other pages

---
+++ src/routes/Routes/Routes.js
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main";
+import About from "../../pages/About/About";
+import Blog from "../../pages/Blog/Blog";
+import Courses from "../../pages/Courses/Courses";
+import FAQ from "../../pages/FAQ/FAQ";
+import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/home',
+                element: <Home></Home>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
+            },
+            {
+                path: '/faq',
+                element: <FAQ></FAQ>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+        ]
+    }
+]);
+
+export default router;
