a1d7dc39b09de2d570291ba2a33aeb9fb3d4e46e
Masud-Ahmed35 <masud.cse.uap@gmail.com>
2022-10-26 16:02:02 +0600

    used environment veriable for secure app keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBR-E0hbom-Ow0jErj4KXHf0cvVfPmQy1Q",
-    authDomain: "secret-web-1e169.firebaseapp.com",
-    projectId: "secret-web-1e169",
-    storageBucket: "secret-web-1e169.appspot.com",
-    messagingSenderId: "887827226445",
-    appId: "1:887827226445:web:38bad408d1b69ca0e4a104"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 const app = initializeApp(firebaseConfig);
